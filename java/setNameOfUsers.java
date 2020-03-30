0271e19d6efb540c836b3ec79af53e8c05845e62
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2020-03-29 22:15:09 -0700

    Add files via upload

---
+++ java/setNameOfUsers.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setNameOfUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_name_of_users);
+    }
+}
---
+++ java/showingOtherUsers.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showingOtherUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showing_other_users);
+    }
+}
